9841c229f325aca0953df5be0cfdcc56d3ffc8ec
Akshit srivastav <akshitsrivastav95@gmail.com>
2025-07-05 20:54:53 +0530

    Fix bind to 0.0.0.0 and use dynamic port

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def submit():
     return "✅ Data sent to your email successfully!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local use
+    app.run(host="0.0.0.0", port=port)
+
